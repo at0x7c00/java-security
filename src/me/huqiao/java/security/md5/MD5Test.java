5199e16ef27c9d90030142d88fafc07e91ec8c81
xooxle <zoozle@qq.com>
2017-10-17 11:23:27 +0800

    MD5

---
+++ src/me/huqiao/java/security/md5/MD5Test.java
@@ -0,0 +1,25 @@
+package me.huqiao.java.security.md5;
+
+import java.security.MessageDigest;
+
+import me.huqiao.java.security.base64.Hex;
+import sun.misc.BASE64Encoder;
+
+public class MD5Test {
+
+	public static void main(String[] args) throws Exception {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		
+		String pwd = "a";
+		
+		byte[] md5Bytes = md.digest(pwd.getBytes("UTF-8"));
+		
+		System.out.println(new String(Hex.encode(md5Bytes)));
+		
+		BASE64Encoder bencoder = new BASE64Encoder();
+		System.out.println(bencoder.encode(md5Bytes));
+		
+		
+		
+	}
+}
